a4788352d2fc9912a18a4979584ef096dbfa4814
grzhan <envy518@gmail.com>
2017-12-20 10:06:03 +0900

    chore: support compile serveral files with Babel

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -5,23 +5,25 @@ import babel from 'gulp-babel';
 import rename from 'gulp-rename';
 
 const LOCAL_ENV_PATH = process.env.HEXO_TAG_APLAYER_LOCAL_PATH;
+const sources = ['./index.es', './lib/*.es'];
+const resetExt = path => { path.ext = '.js' };
 
 gulp.task('default', ['build']);
 
 gulp.task('build', () => {
-    return gulp.src('./index.es')
+    return gulp.src(sources)
         .pipe(babel({
             'presets': ['es2015']
         })) 
-        .pipe(rename('index.js'))
+        .pipe(rename(resetExt))
         .pipe(gulp.dest('.'));
 });
 
 gulp.task('test', () => {
-    return gulp.src('./index.es')
+    return gulp.src(sources)
         .pipe(babel({
             'presets': ['es2015']
         }))
-        .pipe(rename('index.js'))
+        .pipe(rename(resetExt))
         .pipe(gulp.dest(LOCAL_ENV_PATH));
 });
