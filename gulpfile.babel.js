c27c42e06583db97b4c04a101a5687da7f19aab1
grzhan <envy518@gmail.com>
2018-03-02 01:54:10 +0900

    feat(gulp): clean out obsolete files

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,6 +3,7 @@
 import gulp from 'gulp';
 import babel from 'gulp-babel';
 import rename from 'gulp-rename';
+import clean from 'gulp-clean';
 
 const LOCAL_ENV_PATH = process.env.HEXO_TAG_APLAYER_LOCAL_PATH;
 const sources = ['index.es', 'lib/**/*.es', 'common/**/*.es'];
@@ -29,4 +30,10 @@ gulp.task('test', () => {
         }))
         .pipe(rename(resetExt))
         .pipe(gulp.dest(LOCAL_ENV_PATH));
+});
+
+gulp.task('clean', () => {
+    const builds = sources.map((s) => s.replace('es', 'js'));
+    return gulp.src(builds, {read: false})
+        .pipe(clean());
 });
